29319c697c2097a3623ff61c45c3bc2a23116c23
Jonas Wolter <jonas.wolter.ch@gmail.com>
2020-05-21 15:09:11 +0200

    Add Polygon

--- Polygon.mjs
+++ Polygon.mjs
@@ -1,11 +1,22 @@
-import { Circle } from './Circle.mjs';
+
+import { ComplexCollisionShape } from './CollisionDetection.mjs';
+import { Line } from './line.mjs';
 
 export class Polygon{
     constructor(parent, ...points){
-       
+        this._lines = [];
+        let lastPoint = points[points.length-1];
+        for(let point of points){
+            this._lines.push(new Line(parent, lastPoint, point, 4));
+            lastPoint = point;
+            
+        }
+        
+        const collisionShapes = this._lines.map(line => line.getCollisionShape());
+        this._collisionShape = new ComplexCollisionShape("Polygon", collisionShapes);
     }
 
     getCollisionShape(){
-        
+        return this._collisionShape;
     }
 }
