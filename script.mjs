8b0094c431fdd62fe13da5da702ed11dced9ab60
Jonas Wolter <jonas.wolter.ch@gmail.com>
2020-06-07 15:28:27 +0200

    Add Fullscreen

--- script.mjs
+++ script.mjs
@@ -1,3 +1,18 @@
 import { Table } from "./Table.mjs";
+import { Background } from "./Background.mjs";
 
-const table = new Table(document.body, 1000, 1000);
+document.addEventListener('click', (event) => {
+    console.log('Hello');
+    if (document.fullscreenElement) {
+		document.exitFullscreen();
+	} else {
+		document.documentElement.requestFullscreen();
+	}
+});
+// const page = document.documentElement;
+// promise = page.requestFullscreen();
+
+
+new Background(document.body);
+const table = new Table(document.body, 1200, 1900);
+
